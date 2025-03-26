5b95c4259ff3cc2c7862b48fa12c0b7cbee8f402
Sander Balcazar <sander4.rol@gmail.com>
2025-03-25 21:48:03 -0600

    guardando los cambios antes de hacer pull

--- microservicio-clientes/src/main/java/com/example/microservicio_clientes/controller/ClientesController.java
+++ microservicio-clientes/src/main/java/com/example/microservicio_clientes/controller/ClientesController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +23,7 @@ import com.example.microservicio_clientes.services.ClientesServices;
 
 import jakarta.validation.Valid;
 
-
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/clientes")
 public class ClientesController {
